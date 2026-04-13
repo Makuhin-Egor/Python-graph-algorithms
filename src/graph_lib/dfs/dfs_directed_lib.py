c4c7d0f661e8b9574e1648e6c6ecfac9afbe0d4b
Makuhin Egor <makuhinegor219@gmail.com>
2026-04-13 18:09:08 +0300

    feat: add topological_sort function in dfs_directed

--- src/graph_lib/dfs/dfs_directed_lib.py
+++ src/graph_lib/dfs/dfs_directed_lib.py
@@ -17,3 +17,26 @@ def has_cycle_directed(graph):
         color[node] = BLACK
         return False
     return any(dfs(v) for v in all_vertices if color[v] == WHITE)
+
+def topological_sort(graph):
+    if not graph:
+        raise ValueError('Graph is empty!')
+    all_vertices = set(graph.keys())
+    for neighbours in graph.values():
+        all_vertices.update(neighbours)
+    WHITE, GRAY, BLACK = 0, 1, 2
+    color = {v: WHITE for v in all_vertices}
+    order = []
+    def dfs(node):
+        color[node] = GRAY
+        for neighbour in graph.get(node, []):
+            if color[neighbour] == GRAY:
+                raise ValueError('Has cycle!')
+            if color[neighbour] == WHITE:
+                dfs(neighbour)
+        color[node] = BLACK
+        order.append(node)
+    for node in all_vertices:
+        if color[node] == WHITE:
+            dfs(node)
+    return order[::-1]
