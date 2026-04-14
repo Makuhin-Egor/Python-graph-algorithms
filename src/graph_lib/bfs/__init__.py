1b65ec5e8d74d240035ef570e089cd749121f853
Makuhin Egor <makuhinegor219@gmail.com>
2026-04-14 19:30:03 +0300

    chore: fix the init logic for making the library a package

---
+++ src/graph_lib/bfs/__init__.py
@@ -0,0 +1,4 @@
+from .grid_bfs_lib import *
+from .bfs_general_lib import *
+from .bfs_undirected_lib import *
+from .bfs_directed_lib import *
---
+++ src/graph_lib/dfs/__init__.py
@@ -0,0 +1,3 @@
+from .dfs_general_lib import *
+from .dfs_undirected_lib import *
+from .dfs_directed_lib import *
